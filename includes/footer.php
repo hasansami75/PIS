897147cbe6099ca8d524d085d03aec1e8c8ff348
hasansami75 <mehedihasan3175@gmail.com>
2021-01-04 13:28:29 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+ 
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="text-center">Copyright &copy; 2017. All rights reserved to Md. Abir Reza.</p>
+        </div>
+    </footer>
+    
+    <!-- Jquery Link Part Start Here -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <!-- Javascript Link Part Start Here -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
